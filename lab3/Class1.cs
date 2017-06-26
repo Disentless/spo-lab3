c24dcf520906e0ff4aa91d900777ec03fe19677f
James Brown <dim5843@gmail.com>
2017-06-26 17:34:22 +0300

    Initial commit

---
+++ lab3/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public abstract class Converter
+    {
+        public class ConvertException : System.Exception
+        {
+            public override string ToString()
+            {
+                return "Ошибка конвертирования";
+            }
+        }
+
+        public static int Str16ToInt(string str)
+        {
+            int res = 0;
+            for (var i = 0; i < str.Length; ++i)
+            {
+                var ch = str[str.Length - i - 1];
+                int p2 = (int)Math.Pow(16, i);
+                if (ch >= '0' && ch <= '9')
+                {
+                    res += p2 * (ch - '0');
+                }
+                else if (ch >= 'A' && ch <= 'F')
+                {
+                    res += p2 * (ch - 'A' + 10);
+                }
+                else
+                {
+                    throw new ConvertException();
+                }
+            }
+            return res;
+        }
+        public static int Str10ToInt(string str)
+        {
+            int res = 0;
+            for (var i = 0; i < str.Length; ++i)
+            {
+                var ch = str[str.Length - i - 1];
+                int p2 = (int)Math.Pow(10, i);
+                if (ch >= '0' && ch <= '9')
+                {
+                    res += p2 * (ch - '0');
+                }
+                else
+                {
+                    throw new ConvertException();
+                }
+            }
+            return res;
+        }
+        public static int Str2ToInt(string str)
+        {
+            int res = 0;
+            for (var i = 0; i < str.Length; ++i)
+            {
+                var ch = str[str.Length - i - 1];
+                int p2 = (int)Math.Pow(2, i);
+                if (ch == '0' || ch == '1')
+                {
+                    res += p2 * (ch - '0');
+                }
+                else
+                {
+                    throw new ConvertException();
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ lab3_reader/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public abstract class FlashReader
+    {
+        public static string ReadFlash(Dictionary<int,int> mem)
+        {
+            var str = "";
+            foreach (var pair in mem)
+            {
+                str += pair.Key.ToString() + " : " + pair.Value.ToString() + "\n";
+            }
+            return str;
+        }
+    }
+}
